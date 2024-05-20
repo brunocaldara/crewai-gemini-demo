7d535cb563d48794dacee0e8320ea5aab2fb1fd7
Bruno Caldara <bruno.caldara@cesan.com.br>
2024-05-20 16:32:01 -0300

    Cria o agente, tarefa e equipe

--- main.py
+++ main.py
@@ -12,4 +12,32 @@ llm = ChatGoogleGenerativeAI(model="gemini-pro",
                              temperature=0.5,
                              google_api_key=os.getenv("GOOGLE_API_KEY"))
 
-tool_search = DuckDuckGoSearchRun()
+search_tool = DuckDuckGoSearchRun()
+
+search_agent = Agent(
+    role="Pesquisador",
+    goal="Pesquisar sobre as melhores matérias sobre o assunto {topic} no Brasil em 2024",
+    backstory="Você é um pesquisador senior responsável por elaborar uma pesquisa sobre o tema abordado",
+    allow_delegation=False,
+    cache=True,
+    verbose=True,
+    tool=[search_tool],
+    llm=llm
+)
+
+search_task = Task(
+    description="Buscar as matérias mais importantes sobre {topic} no Brasil em 2024",
+    expected_output="Uma lista enumerada com as matérias e o link mais relevantes matérias sobre o assunto pesquisado",
+    agent=search_agent
+)
+
+crew = Crew(
+    tasks=[search_task],
+    agents=[search_agent],
+    verbose=True,
+    cache=True,
+    process=Process.sequential
+)
+
+result = crew.kickoff(inputs={"topic": "Mercado de trabalho com Python"})
+print(result)
